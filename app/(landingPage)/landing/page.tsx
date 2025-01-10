628bd1ad525ce8c713161c407d47f519c7f88985
vadao <adao_vivaldo@hotmail.com>
2025-01-10 22:03:24 +0100

    correcting the page landing

--- app/(landingPage)/landing/page.tsx
+++ app/(landingPage)/landing/page.tsx
@@ -8,7 +8,7 @@ import { useCarousel } from "@/hooks/useCarousel";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useGetCoursesQuery } from "@/state/api";
 import { useRouter } from "next/navigation";
-import CourseCardSearch from "@/components/CourseCardSearch";
+import CourseCardSearch from "@/components/course/CourseCardSearch";
 import { useUser } from "@clerk/nextjs";
 
 const LoadingSkeleton = () => {
